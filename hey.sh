840fc65e528ceda36cc0da867e7dd13e66a14040
98WuG <gwu02251250@gmail.com>
2016-12-25 23:01:20 -0500

    Script now reads in lyrics from text files

--- hey.sh
+++ hey.sh
@@ -8,42 +8,38 @@ function lyric {
 	if [[ $response != $2 ]];
 	then
 		echo "Clearly you have not heard this new song. It's called" 
-		figlet "Closer by the Chainsmokers"
+		figlet "$songtitle"
 		exit 2
 	fi
 }
 
-# Initial prompt
+# Initial prompt for song name
 echo -n "Hey, have you heard this new song? It's called: "
 read songtitle
-if [[ $songtitle != "Closer by The Chainsmokers" ]]
+
+# Convert song name to filename
+filename="$(echo $songtitle | sed -r 's/ /_/g').txt"
+if [ ! -f ./$filename ];
 then
 	echo "Clearly you have not heard this new song."
 	exit 1
 fi
-echo -e "\n\e[1m\e[4mCloser by The Chainsmokers\e[0m"
+echo -e "\n\e[1m\e[4m$songtitle\e[0m"
 
-# Lyrics test
-lyric "Hey, I was doing just fine before I met you" "I drink too much and that's an issue"
-lyric "But I'm okay." "Hey, you tell your friends it was nice to meet them"
-lyric "But I hope I never see them again" "I know it breaks your heart"
-lyric "Moved to the city in a broke down car and" "Four years no calls"
-lyric "Now you're looking pretty in a hotel bar" "And I-I-I-I-I can't stop"
-lyric "No, I-I-I-I-I can't stop." "So baby pull me closer"
-lyric "In the backseat of your rover" "That I know you can't afford"
-lyric "Bite that tattoo on your shoulder" "Pull the sheets right off the corner"
-lyric "Of the mattress that you stole" "From your roommate back in boulder"
-lyric "We ain't ever getting older" "Doo doo DOO doodoodoo doodoo doodoo DOO doodoo doodoodoo"
-lyric "You look as good as the day I met you" "I forget just why I left you, I was insane"
-lyric "Stay and play that Blink-182 song" "That we beat to death in Tuscon, okay"
-lyric "I know it breaks your heart" "Moved to the city in a broke down car and"
-lyric "Four years no calls" "Now I'm looking pretty in a hotel bar"
-lyric "And I-I-I-I-I can't stop" "No, I-I-I-I-I can't stop"
-lyric "So baby pull me closer" "In the backseat of your rover"
-lyric "That I know you can't afford" "Bite that tattoo on you shoulder"
-lyric "Pull the sheets right off the corner" "Of the mattress that you stole"
-lyric "From your roommate back in boulder" "We ain't ever getting older"
+# Read in file two lines at a time and store in two arrays
+# @kevingeng yes I know this is inefficient but I don't know how to code so fight me
+lines=()
+corrects=()
+while read -r line; read -r correct
+do
+	lines+=("$line")
+	corrects+=("$correct")
+done < "$filename"
+
+# Use lines to lyric test
+for ((i=0; i<=${#lines[@]}; i++)); do
+	lyric "${lines[i]}" "${corrects[i]}"
+done
 
 # Passed
-echo "Doo doo DOO doodoodoo doodoo doodoo DOO doodoo doodoodoo"
 echo "I guess you have heard this new song."
